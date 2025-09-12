58359393c1dca54c2951d8ed56d4b6e80f91f884
Anna-5555 <anna.khaybra@mail.ru>
2025-09-12 15:47:01 +0300

    Added changes to conftest.py

--- conftest.py
+++ conftest.py
@@ -9,7 +9,8 @@ headers = {"User-Agent": "Mozilla/5.0 ("
            "Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ("
            "KHTML, like Gecko) "
                          "Chrome/134.0.0.0 YaBrowser/25.4.0.0 Safari/537.36",
-           "authorization": auth_token}
+           "authorization": auth_token
+           }
 
 
 @pytest.fixture
